17743f73e3aae3d6293bf6971b0da21904f95e1f
Carlos Alvarado <tec.carlos.a@gmail.com>
2024-10-07 22:57:04 -0500

    Add form group register and validators to Header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      nombre: ['', Validators.required],
+      direccion: ['', Validators.required],
+      celular: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      vehiculoMarca: ['', Validators.required],
+      vehiculoModelo: ['', [Validators.required, Validators.min(1900)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmarPassword: ['', Validators.required]
+    });
+  }
+
+  register() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      // Aquí puedes realizar el envío de los datos al backend
+      // Ejemplo: this.http.post('url_backend', this.registerForm.value).subscribe();
+    } else {
+      console.log('Formulario no válido');
+    }
+  }
 
   ngOnInit(): void {
   }
