9404a5a2784c3a14a5573303675bfeb781831595
Diego.ruiz <dianruro8223@otmail.com>
2024-10-20 15:47:14 -0500

    add new component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
+import { UserComponent } from './components/user/user.component';
+import { TecComponent } from './components/tec/tec.component';
+import { ContentComponent } from './components/content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'content', component: ContentComponent},
+  { path: 'admin', component: AdminComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'tecnico', component: TecComponent },
+  { path: '', redirectTo: '/content', pathMatch: 'full' }, // Redireccionar a user por defecto
+  { path: '**', redirectTo: '/content' } // Redireccionar a user si la ruta no existe
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
