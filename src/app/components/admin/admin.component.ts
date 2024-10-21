751e737ffa4cc480039f9862981e8a85abda7830
Carlos Alvarado <tec.carlos.a@gmail.com>
2024-10-21 18:08:29 -0500

    Add Tec array

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
+  img: string = "../../../assets/img/mecanico"
+
+  mecanicos: any = [
+    {
+      image: `${this.img}1.jpg`,
+      name: 'Carlos Albeiro Martinez',
+      ocupation: 'Mecanico de pista 1.',
+      btn1: 'Ver',
+      btn2: 'Editar'
+    },
+    {
+      image: `${this.img}2.jpg`,
+      name: 'Santiago alberto hoyos',
+      ocupation: 'Mecanico de pista 2.',
+      btn1: 'Asignar',
+      btn2: 'Modificar'
+    },
+    {
+      image: `${this.img}3.jpg`,
+      name: 'Cristian Sandoval',
+      ocupation: 'Tecnico en Sincronizacion.',
+      btn1: 'Eliminar',
+      btn2: 'Crear'
+    },
+    {
+      image: `${this.img}4.jpg`,
+      name: 'Sergio Arango',
+      ocupation: 'Supervisor de Area.',
+      btn1: 'Mantener',
+      btn2: 'Sacar'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
+    this.mecanicos.map((x:any) => {
+      console.log(x.name)
+    })
   }
 
 }
